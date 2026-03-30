1583f793494fac60e7f4a5f86d9c518fde4c2083
Brian Thomson <nvcloud221@gmail.com>
2026-03-30 18:01:56 +0100

    Added  to easily configure the window manager

---
+++ setup.sh
@@ -0,0 +1,5 @@
+cp ./setup/sewm_exec /usr/bin/sewm_exec
+cp ./setup/sewm /usr/bin/sewm
+cp ./setup/sewm.desktop /usr/share/xsessions/sewm.desktop
+cp ./setup/config.dat ~/.sewm.config
+cp background.png ~/.sewm.background.png
